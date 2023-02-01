6586f954cc6018bbeb78ac03a570b9d591c2f473
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-01 18:00:50 -0500

    Update run.pl

--- run.pl
+++ run.pl
@@ -1,12 +1,16 @@
 #!/usr/bin/perl -w
 print "\033[4;37mR\033[0mun, e\033[4;37mx\033[0mit, or \033[4;37mM\033[0manpage? ";
 $option = <>;
-if($option =~ "r"){
+chomp($option);
+if(lc($option) eq "r"){
   system("./ai.rb");
 }
-elsif($option =~ "e"){
+elsif(lc($option) eq "e"){
   exit;
 }
-elsif($option =~ "m"){
+elsif(lc($option) eq "m"){
   system("man ./airb.1");
+}
+else{
+  print "I don't know that."
 }
