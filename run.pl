82aad21de239095461ce1b982649fcf51830c3f5
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-02 22:07:45 +0000

    Pending changes exported from your codespace

--- run.pl
+++ run.pl
@@ -3,13 +3,13 @@ print "\033[4;37mR\033[0mun, e\033[4;37mx\033[0mit, or \033[4;37mM\033[0manpage?
 $option = <>;
 chomp($option);
 if(lc($option) eq "r"){
-  system("./ai.rb");
+  system("./lexi.rb");
 }
 elsif(lc($option) eq "x"){
   exit;
 }
 elsif(lc($option) eq "m"){
-  system("man ./airb.1");
+  system("man ./lexi.1");
 }
 else{
   print "I don't know that.\n"
