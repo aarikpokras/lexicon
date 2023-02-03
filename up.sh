7a5db3a520239574bdc9cff69972cc03a6847ef1
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-03 18:43:23 -0500

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,1 +1,2 @@
-if [ $(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/VERSION) != "230" ];then echo "Please update LexiCon."; else echo "LexiCon is up to date."; fi
+VER="230"
+if [ $(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/VERSION) != "$VER" ];then echo "Please update LexiCon."; else echo "LexiCon is up to date. (v$VER)"; fi
