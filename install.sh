9dfd9c8632ff847006cec49320325bec68164ed6
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-04 16:47:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #FriFeb3
-curl -Lo lx.zip https://github.com/aarikpokras/lexicon/archive/refs/heads/main.zip && rsync -a lx.zip /usr/local/bin && cd /usr/local/bin && unzip -o lx.zip && cd lexicon-main && rsync -a * ..
+curl -Lo lx.zip https://github.com/aarikpokras/lexicon/archive/refs/heads/main.zip && rsync -a lx.zip /usr/local/bin && cd /usr/local/bin && unzip -o -qq lx.zip && cd lexicon-main && rsync -a * ..
+printf '\033[1;32mDone!\033[0m\n'
 # You're gonna have to wait a little bit for the command to work
