a5691ce2e6fa9c5e4473ada36bf3c76e5a5eae6d
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-03 20:49:38 -0500

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
-VER="232"
-THX_VAR='sudo /bin/bash -c \"\$\(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/install.sh\)\"'
+VER="233"
+THX_VAR='sudo /bin/bash -c "$(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/install.sh)"'
 if [ $(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/VERSION) != "$VER" ];then printf "Please update LexiCon. You can run\n\n$THX_VAR\n\nto update."; else echo "LexiCon is up to date. (v$VER)";echo "DEBUG:";echo "PC: $(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/VERSION)"; fi
 # Keep in mind that GitHub Raw takes five minutes to propagate (or rather to un-cache)
