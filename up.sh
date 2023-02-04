eede34b931d6418264a7a16fe6d3bca51187693d
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-04 16:32:23 -0500

    Update up.sh

--- up.sh
+++ up.sh
@@ -4,5 +4,5 @@ THX_VAR='sudo /bin/bash -c "$(curl https://raw.githubusercontent.com/aarikpokras
 CHANGEL=$(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/CL_NU)
 
 __LNKVR=$(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/VERSION)
-if [ $__LNKVR != "$VER" ];then printf "Please update LexiCon. You can run\n\n$THX_VAR\n\nto update.\nDEBUG:\n$__LNKVR\n$CHANGEL\n"; else echo "LexiCon is up to date. (v$VER)";echo "DEBUG:";echo "PC: $__LNKVR\n"; fi
+if [ $__LNKVR != "$VER" ];then printf "(v$VER)\nPlease update LexiCon. You can run\n\n$THX_VAR\n\nto update.\nDEBUG:\n$__LNKVR\n$CHANGEL\n"; else echo "LexiCon is up to date. (v$VER)";echo "DEBUG:";echo "PC: $__LNKVR\n"; fi
 # Keep in mind that GitHub Raw takes five minutes to propagate (or rather to un-cache)
