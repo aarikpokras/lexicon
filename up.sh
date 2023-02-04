0a6f094e1b22b99f0fd6edb3245ea60d4b43f5d9
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-04 17:43:17 -0500

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
-VER="323"
+VER="324"
 
 THX_VAR='sudo /bin/bash -c "$(curl https://raw.githubusercontent.com/aarikpokras/lexicon/main/install.sh)"'
 CHANGEL=$(curl --silent https://raw.githubusercontent.com/aarikpokras/lexicon/main/CL_NU)
